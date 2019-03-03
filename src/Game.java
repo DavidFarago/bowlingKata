2dfff90dec860c9ca228a5ac0735ab1daec63e0f
David Farago <farago@qpr-technologies.de>
2019-03-03 14:52:52 +0100

    basic setup of Game class with tests (and idea config)

---
+++ src/Game.java
@@ -0,0 +1,10 @@
+public final class Game {
+
+    public void roll(int pins) {
+
+    }
+
+    public int score(){
+        return 0;
+    }
+}
---
+++ tests/GameTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    private Game sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new Game();
+    }
+
+    @Test
+    void simpleGames() {
+
+    }
+
+}
