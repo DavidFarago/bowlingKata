08b7efd973de0c80730a0302b84b213ec85e7fee
David Farago <farago@qpr-technologies.de>
2019-03-03 15:02:32 +0100

    sequence of rolls with 0 pins each; documentation

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,18 @@
+/**
+ * Bowling kata with shippable quality:
+ * * outer quality: correct, robust, usable
+ * * inner quality: maintainable, extensible, readable (especially bowling "business rules").
+ */
 public final class Game {
 
     public void roll(int pins) {
 
+    }
+
+    public void rolls(int ... pinsArray) {
+        for (int pins : pinsArray) {
+            roll(pins);
+        }
     }
 
     public int score(){
